f9393a8989b361e1bee453b86864ab02bb909996
Eduardo Joly <eduardo.niceinfo@gmail.com>
2022-05-02 20:29:41 -0300

    acabou

--- Desafio.js
+++ Desafio.js
@@ -6,10 +6,10 @@ for (let contador = 1; contador < listadeAlunos.length; contador++) {
     for (let nome of listadeAlunos) {
 
     if (contador % 2 == 0) {
-        console.log(`${nome} está na posição ${contador} e este número é par`)
+        console.log(`${nome} está na posição ${contador}, e este número é par`)
     }
     else{
-        console.log(`${nome} está na posição ${contador} e este número é impar`)
+        console.log(`${nome} está na posição ${contador}, e este número é impar`)
     }
     contador++
 }      
